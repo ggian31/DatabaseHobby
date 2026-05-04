78957cd89cd3a3362d43d670748442485b39488b
ggian31 <giancarlo.guillenc@gmail.com>
2026-05-03 20:12:32 -0500

    Increase Game Boy listing limit to 300

--- consoles/gb/gb.js
+++ consoles/gb/gb.js
@@ -6,6 +6,7 @@ window.ConsoleConfig.push({
     color: '#8b5cf6', // A purple gradient similar to what we used, or #8bac0f for classic GB
     badgeBg: '#8b5cf6',
     badgeColor: 'white',
+    itemsPerPage: 300,
     paths: ['/Archivos/001 Instaladores/Juegos/Emuladores', '/Documents/Archivos/001 Instaladores/Juegos/Emuladores'],
     targets: [ 
         { name: 'gb', platform: 'Gameboy', img: 'downloaded_images' }, 
