de6e73a8e49293a8cb70180a2bbc9bd25aadcbb9
Giancarlo De Jesus Guillen Cotrina <gguillen@marsa.com.pe>
2026-05-05 14:44:52 -0500

    Set GBA items per page to 300

--- consoles/gba/gba.js
+++ consoles/gba/gba.js
@@ -6,5 +6,6 @@ window.ConsoleConfig.push({
     color: '#4f46e5',
     badgeBg: '#4f46e5',
     badgeColor: 'white',
+    itemsPerPage: 300,
     paths: ['/Archivos/001 Instaladores/Juegos/Emuladores/gba', '/Documents/Archivos/001 Instaladores/Juegos/Emuladores/gba']
 });
