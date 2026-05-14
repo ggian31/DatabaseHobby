2c639c926cf7a244a7f574a5d655edec98f7cf6c
ggian31 <giancarlo.guillenc@gmail.com>
2026-05-14 12:50:30 -0500

    Configure NDS console support, fix paths, and update UI icon/color

---
+++ consoles/nds/nds.js
@@ -0,0 +1,11 @@
+window.ConsoleConfig = window.ConsoleConfig || [];
+window.ConsoleConfig.push({
+    id: 'NDS',
+    name: 'Nintendo DS',
+    icon: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="white" style="vertical-align: middle; margin-top: -2px;"><rect x="4" y="2" width="16" height="9" rx="3"/><rect x="4" y="13" width="16" height="9" rx="3"/><rect x="7" y="4" width="10" height="5" rx="1" fill="black" fill-opacity="0.2"/><rect x="7" y="15" width="10" height="5" rx="1" fill="black" fill-opacity="0.2"/><circle cx="18" cy="18" r="1" fill="black" fill-opacity="0.2"/><circle cx="6" cy="18" r="1" fill="black" fill-opacity="0.2"/></svg>',
+    color: '#8b0000',
+    badgeBg: '#8b0000',
+    badgeColor: 'white',
+    itemsPerPage: 300,
+    paths: ['/Archivos/001 Instaladores/Juegos/Emuladores/nds', '/Documents/Archivos/001 Instaladores/Juegos/Emuladores/nds'],
+});
